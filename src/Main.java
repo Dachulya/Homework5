3ced38a72feea7c7e0016ad90e747f148b2deb16
Дарья <dnovogodniaya@ya.ru>
2022-09-06 22:06:43 +0300

    nomber1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int clientOS=0;
+        switch (clientOS){
+            case 0:
+                System.out.println("Устаовите версию приложения для IOS поссылке");
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        //Задание2
+
+
+    }
+}
