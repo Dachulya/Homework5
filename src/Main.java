88fd8f7a63c2b21d159bc0f044ecbb5b15c22965
Дарья <dnovogodniaya@ya.ru>
2022-09-07 00:51:26 +0300

    number2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 1
-        int clientOS=0;
+        //Задание 1//Задание 2
+        int clientOS=1;
+        int a=2016;
         switch (clientOS){
             case 0:
                 System.out.println("Устаовите версию приложения для IOS поссылке");
+                if (a<2015) {
+                    System.out.println("Установите упрощённую версию для IOS");
+            }
             case 1:
                 System.out.println("Установите версию приложения для Android по ссылке");
+                if (a<2015) {
+                    System.out.println("Установите упрощённую версию для Android");
+                }
         }
-        //Задание2
+
 
 
     }
